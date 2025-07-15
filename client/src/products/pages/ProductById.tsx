cacd2b632081f39edb13b000ee175af23bb64ca5
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2025-07-15 20:54:54 -0300

    scroll to top added on product detail view

--- client/src/products/pages/ProductById.tsx
+++ client/src/products/pages/ProductById.tsx
@@ -1,10 +1,16 @@
 import { useParams } from "react-router-dom";
 import { ProductCard, useProduct } from "..";
+import { useEffect } from "react";
 
 export const ProductById = () => {
   const { id } = useParams();
 
   const { product, isLoading } = useProduct({ id: +id! });
+  //este efecto solo sirve para poder enviar la pantalla hacia arriba cuando se monta el componente
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Producto:</h1>

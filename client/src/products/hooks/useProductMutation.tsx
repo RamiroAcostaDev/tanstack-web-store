fe2b7eb43f5bb03a8f910ac3b317e767d6ed65c3
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2025-07-17 23:47:55 -0300

    refresh women product list with setQueryData after adding a new product

--- client/src/products/hooks/useProductMutation.tsx
+++ client/src/products/hooks/useProductMutation.tsx
@@ -1,15 +1,23 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { productActions } from "..";
+import { Product, productActions } from "..";
 
 export const useProductMutation = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({
-        queryKey: ["products", { filterKey: data.category }],
-      });
+    onSuccess: (product) => {
+      //ejemplo de invalidacion de query, para que vuelva a hacer la peticion en la pagina respectiva y agregue los nuevos productos automaticamente (sin refrescar la pagina)
+      //   queryClient.invalidateQueries({
+      //     queryKey: ["products", { filterKey: data.category }],
+      //   });
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: product.category }],
+        (old) => {
+          if (!old) return [product];
+          return [...old, product];
+        }
+      );
     },
   });
   return mutation;
